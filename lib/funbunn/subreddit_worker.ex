d51bc4da4e4065970c0e89c9961715cd8b9630c8
Daven Casia <77761194+fmterrorf@users.noreply.github.com>
2022-10-01 21:56:05 -0600

    Use `?before=<thread_name>` instead of using the thread ids

--- lib/funbunn/subreddit_worker.ex
+++ lib/funbunn/subreddit_worker.ex
@@ -2,7 +2,7 @@ defmodule Funbunn.SubredditWorker do
   use GenServer, restart: :temporary
   require Logger
 
-  @poll_interval :timer.minutes(5)
+  @poll_interval :timer.minutes(1)
 
   def start_link(subreddit) do
     GenServer.start_link(__MODULE__, subreddit)
@@ -12,32 +12,28 @@ defmodule Funbunn.SubredditWorker do
   def init(subreddit) do
     Logger.info("Starting #{__MODULE__} for #{subreddit}")
     send(self(), :poll)
-    {:ok, {subreddit, ids_from_store(subreddit)}}
+    {:ok, {subreddit, thread_name_from_store(subreddit)}}
   end
 
   @impl true
-  def handle_info(:poll, {subreddit, ids} = state) do
+  def handle_info(:poll, {subreddit, last_thread_name} = state) do
     state =
-      with {:ok, entries} <- Funbunn.Api.fetch_new_entries(subreddit) do
-        latest_ids = Enum.map(entries, fn item -> item.id end)
-        ids_to_send = extract_ids_for_sending(latest_ids, ids)
+      with {:ok, entries = [%{name: latest_thread_name} | _]} <-
+             Funbunn.Api.fetch_new_entries(subreddit, last_thread_name) do
+        send_to_discord(entries, subreddit)
 
-        if ids_to_send != [] do
-          Enum.filter(entries, fn item -> item.id in ids_to_send end)
-          |> maybe_send_to_discord(subreddit, ids)
+        Funbunn.Store.insert_subreddit!(%{
+          name: subreddit,
+          last_thread_name_seen: latest_thread_name
+        })
 
-          Funbunn.Store.insert_subreddit!(%{
-            name: subreddit,
-            thread_ids: latest_ids
-          })
-        else
-          Logger.info("No new posts found for subreddit: #{subreddit}")
-        end
-
-        {subreddit, latest_ids}
+        {subreddit, latest_thread_name}
       else
+        {:error, reason} ->
+          Logger.error("Fetching new entries failed. Reason: #{inspect(reason)}")
+
         _ ->
-          Logger.warn("Fetching new entries failed. Skipping...")
+          Logger.info("No new entries found for subreddit: #{subreddit}. Skipping...")
           state
       end
 
@@ -68,36 +64,20 @@ defmodule Funbunn.SubredditWorker do
 
   # Helpers
 
-  defp extract_ids_for_sending(latest_ids, old_ids) do
-    MapSet.new(latest_ids)
-    |> MapSet.difference(MapSet.new(old_ids))
-    |> MapSet.to_list()
-  end
-
-  defp ids_from_store(subreddit) do
+  defp thread_name_from_store(subreddit) do
     if sub = Funbunn.Store.subreddit(subreddit) do
-      sub.thread_ids
-    else
-      []
+      sub.last_thread_name_seen
     end
   end
 
-  defp maybe_send_to_discord(items = [_ | _], subreddit, _ids = [_ | _]) do
+  defp send_to_discord(items = [_ | _], subreddit) do
     messages = Funbunn.DiscordBody.new(items)
-    Logger.debug("Sending #{length(messages)} to SubredditWorker")
+    Logger.debug("Sending #{length(messages)} to #{subreddit} SubredditWorker")
 
     Enum.each(messages, fn messages ->
       {_, id} = key = {:messages, Ecto.UUID.generate()}
       Funbunn.Cache.insert(key, messages)
       Funbunn.SubredditWorker.publish(subreddit, {:deliver, id})
     end)
-  end
-
-  # defp maybe_send_to_discord(_items, _subreddit, _ids = []) do
-  #   Logger.debug("Initial run of #{__MODULE__}. Skip sending messages to discord")
-  # end
-
-  defp maybe_send_to_discord(_items, _subreddit, _ids) do
-    Logger.debug("No new items found. Skipping")
   end
 end
